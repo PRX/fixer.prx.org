bf7f96dc3b03df023e8e62b3f50d162027eff31f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-01-23 15:58:34 -0500

    stub publishing messages

--- app/models/base_model.rb
+++ app/models/base_model.rb
@@ -4,4 +4,8 @@ class BaseModel < ActiveRecord::Base
   self.abstract_class = true
 
   include FixerConstants
+
+  # stub for now while async messaging gets sorted out
+  def publish(*args)
+  end
 end
