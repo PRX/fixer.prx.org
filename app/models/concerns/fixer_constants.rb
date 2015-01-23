e0029dd2595b74cb68efc8967df7c11c5b60c0f6
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-01-23 14:43:24 -0500

    move constants from lib to a concern

---
+++ app/models/concerns/fixer_constants.rb
@@ -0,0 +1,14 @@
+module FixerConstants
+  CREATED     = 'created'     # When task first created
+  PROCESSING  = 'processing'  # When task first picked up by a processor
+  COMPLETE    = 'complete'    # task has completed succcessfully
+  ERROR       = 'error'       # task failed for some reason, but could be retried.
+  RETRYING    = 'retrying'    # After an error, when the task has been sent to a queue to try again (like created)
+  CANCELLED   = 'cancelled'   # Manually cancelled, no more retries, or callbacks
+
+  PROGRESS    = 'progress'    # update with the current % of progress for the task, does not change task state
+  INFO        = 'info'        # info logging does not change status of the task
+
+  STATUS_VALUES = [CREATED, PROCESSING, COMPLETE, ERROR, RETRYING, CANCELLED].freeze
+  LOG_STATUS_VALUES = ([PROGRESS, INFO] + STATUS_VALUES).freeze
+end
