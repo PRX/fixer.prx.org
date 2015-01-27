5a11f09643fcc6c01b34ae76782eb8429d31059f
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-01-26 23:38:55 -0500

    installing doorkeeper

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,4 +2,6 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :recoverable, :rememberable, :validatable
+
+  has_many :oauth_applications, class_name: 'Doorkeeper::Application', as: :owner
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,9 @@
 Rails.application.routes.draw do
 
+  use_doorkeeper do
+    controllers applications: 'oauth/applications'
+  end
+
   devise_for :users
 
   resources :jobs
