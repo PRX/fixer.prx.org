1513733e01e56f2e0475d5896936f3a1c2135a74
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-01-26 23:46:24 -0500

    add basic api controller

---
+++ app/controllers/api/api_controller.rb
@@ -0,0 +1,13 @@
+module Api
+  class ApiController < ApplicationController
+    skip_before_filter :verify_authenticity_token
+    skip_before_filter :authenticate
+    before_action :doorkeeper_authorize!
+    respond_to :json
+
+    # Find the user that owns the access token
+    def current_resource_owner
+      User.find(doorkeeper_token.resource_owner_id) if doorkeeper_token
+    end
+  end
+end
