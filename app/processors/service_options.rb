b8e76cd537fdff969f6d8bccdb67354505a29dcc
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-10 11:57:22 -0500

    move and improve service options

---
+++ app/processors/service_options.rb
@@ -0,0 +1,88 @@
+require 'yaml'
+require 'erb'
+require 'uri'
+require 'active_support/core_ext/hash/indifferent_access'
+
+module ServiceOptions
+
+  def self.env=(e)
+    @env = e
+  end
+
+  def self.env
+    @env ||= (defined?(Rails) && Rails.respond_to?(:env) && Rails.env) ||
+    ENV['RAILS_ENV'] || ENV['APP_ENV'] ||
+    'development'
+  end
+
+  def self.root=(r)
+    @root = r
+  end
+
+  def self.root
+    @root ||= (defined?(Rails) && Rails.respond_to?(:root) && Rails.root) ||
+    ENV['APP_ROOT'] ||
+    File.expand_path(File.join(File.dirname(__FILE__), '..'))
+  end
+
+  def self.file_path=(fp)
+    @file_path = fp
+  end
+
+  def self.file_path
+    @file_path ||= "#{root}/config/services.yml"
+  end
+
+  def self.options=(opts)
+    @options = opts
+  end
+
+  def self.options
+    @options ||= parse_service_options || {}
+  end
+
+  def self.service_options(service)
+    service = storage_provider_abbr_for_uri(service) if service.is_a?(URI)
+    options[service]
+  end
+
+  def self.options_for_uri(uri)
+    options = nil
+    # see if there is a user and pwd
+    key      = URI.decode(uri.user) if uri.user
+    secret   = URI.decode(uri.password) if uri.password
+    provider = storage_provider_for_uri(uri)
+    abbr     = storage_provider_abbr_for_uri(uri)
+    if key && secret && provider && abbr
+      options = {
+        :provider => provider,
+        "#{abbr}_access_key_id".to_sym => key,
+        "#{abbr}_secret_access_key".to_sym => secret
+      }.with_indifferent_access
+    end
+    options
+  rescue
+    nil
+  end
+
+  def self.parse_service_options
+    so = YAML::load(ERB.new(File.read(file_path)).result).with_indifferent_access
+    so[env].with_indifferent_access if so
+  end
+
+  def self.storage_provider_for_uri(u)
+    case u.scheme
+    when 's3' then 'AWS'
+    when 'ia' then 'InternetArchive'
+    else nil
+    end
+  end
+
+  def self.storage_provider_abbr_for_uri(u)
+    case u.scheme
+    when 's3' then :aws
+    when 'ia' then :ia
+    else nil
+    end
+  end
+end
---
+++ test/processors/service_options_test.rb
@@ -0,0 +1,82 @@
+require 'test_helper'
+
+class ServiceOptionsTest < ActiveSupport::TestCase
+
+  it 'provides env' do
+    ServiceOptions.env.must_equal 'test'
+  end
+
+  it 'provides env' do
+    ServiceOptions.env = 'foo'
+    ServiceOptions.env.must_equal 'foo'
+    ServiceOptions.env = nil
+    ServiceOptions.env.must_equal 'test'
+  end
+
+  it 'provides root path' do
+    ServiceOptions.root.wont_be_nil
+  end
+
+  it 'can reload root' do
+    ServiceOptions.root.wont_be_nil
+    r = ServiceOptions.root
+    ServiceOptions.root = nil
+    ServiceOptions.root.must_equal r
+  end
+
+  it 'can set the root' do
+    ServiceOptions.root = '/foo'
+    ServiceOptions.root.must_equal '/foo'
+    ServiceOptions.root = nil
+  end
+
+  it 'has yml file path to load' do
+    ServiceOptions.file_path.must_match /services\.yml/
+  end
+
+  it 'can set yml file path' do
+    ServiceOptions.file_path = 'foo.yml'
+    ServiceOptions.file_path.must_equal 'foo.yml'
+    ServiceOptions.file_path = nil
+  end
+
+  it 'provides env' do
+    ServiceOptions.env = 'foo'
+    ServiceOptions.env.must_equal 'foo'
+    ServiceOptions.env = nil
+    ServiceOptions.env.must_equal 'test'
+  end
+
+
+  it 'gets provider for uri' do
+    uri = URI.parse('s3://test.aws.amazon.com/test/test.mp3')
+    provider = ServiceOptions.storage_provider_abbr_for_uri(uri)
+    provider.must_equal :aws
+  end
+
+  it 'can get service options by name' do
+    opts = ServiceOptions.service_options('aws')
+    opts['provider'].must_equal 'AWS'
+    opts['path_style'].must_equal true
+    opts['aws_access_key_id'].must_equal 'S3ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  end
+
+  it 'can get service options by url' do
+    opts = ServiceOptions.service_options(URI.parse('s3://test.aws.amazon.com/test/test.mp3'))
+    opts['provider'].must_equal 'AWS'
+    opts['path_style'].must_equal true
+    opts['aws_access_key_id'].must_equal 'S3ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  end
+
+  it 'can extract options from uri' do
+    uri = URI.parse('s3://KEY:secret@test.aws.amazon.com/test/test.mp3')
+    opts = ServiceOptions.options_for_uri(uri)
+    opts['aws_access_key_id'].must_equal 'KEY'
+    opts['aws_secret_access_key'].must_equal 'secret'
+  end
+
+  it 'can parse service options from a file' do
+    opts = ServiceOptions.parse_service_options
+    opts['aws']['aws_access_key_id'].must_equal 'S3ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+  end
+end
