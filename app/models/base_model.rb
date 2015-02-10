c8d35747204a00f482d3369a922bc9a7a31293ae
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-10 11:58:13 -0500

    make json include root class name

--- app/models/base_model.rb
+++ app/models/base_model.rb
@@ -1,11 +1,9 @@
 # encoding: utf-8
 
 class BaseModel < ActiveRecord::Base
+
+  self.include_root_in_json = true
   self.abstract_class = true
 
   include FixerConstants
-
-  # stub for now while async messaging gets sorted out
-  def publish(*args)
-  end
 end
