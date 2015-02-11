2587b9d2d7f3d60ad96d1032c1cf9f4e4d492c1d
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-11 13:26:17 -0500

    start of webhook handlers

---
+++ app/workers/web_hook_update_worker.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+require 'base_worker'
+
+class WebHookUpdateWorker < BaseWorker
+  def process(web_hook)
+  end
+end
---
+++ app/workers/web_hook_worker.rb
@@ -0,0 +1,8 @@
+# encoding: utf-8
+
+require 'base_worker'
+
+class WebHookWorker < BaseWorker
+  def process(web_hook)
+  end
+end
