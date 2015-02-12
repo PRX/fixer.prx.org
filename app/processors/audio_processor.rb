21ad30d63b285202447ae5cb9c83f0e6453aa8d6
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-12 08:58:26 -0500

    audio processing start

---
+++ app/processors/audio_processor.rb
@@ -0,0 +1,261 @@
+require 'google_speech'
+
+class AudioProcessor < BaseProcessor
+
+  SUPPORTED_FORMATS = ['aac', 'aif', 'aiff', 'alac', 'flac', 'm4a', 'm4p', 'mp2', 'mp3', 'mp4', 'ogg', 'raw', 'spx', 'wav', 'wma']
+  WAVEFORM_WIDTH_MIN = 1800
+
+  attr_accessor :wav_pcm_tempfile
+
+  task_types ['transcode', 'copy', 'analyze', 'validate', 'cut', 'wrap', 'transcribe', 'waveformjson', 'tone_detect', 'silence_detect', 'slice']
+
+  # make sure to handle 64 kb limit on transcripts
+  # perhaps put the transcript/metadata as a result file rather than a response?
+  def transcribe_audio
+    source_wav = get_wav_from_source
+
+    transcript = google_transcribe(source_wav, options)
+
+    base_file_name = File.basename(source.path) + '.json'
+    temp_file= AudioMonster.create_temp_file(base_file_name, false)
+    temp_file.write transcript.to_json
+    temp_file.fsync
+
+    self.destination_format = 'json'
+    self.destination  = temp_file
+    info = info_for_transcript(transcript)
+
+    completed info, 'transcribe audio complete'
+  end
+
+  def info_for_transcript(t)
+    text = t.collect{|i| i[:text]}.join(' ')
+    avg = t.collect{|i| i[:confidence]}.inject(0.0) { |sum, el| sum + el.to_f } / t.size
+
+    {
+      average_confidence: avg,
+      word_count: text.split.size,
+      character_count: text.size
+    }
+  end
+
+  def google_transcribe(wav, opts={})
+    GoogleSpeech.logger = logger
+    GoogleSpeech::Transcriber.new(File.open(wav.path), symbolize_options(opts)).transcribe
+  end
+
+  def waveformjson_audio
+    source_wav = get_wav_from_source
+
+    if options['width_per_second']
+      min = options['width_minimum'] && options['width_minimum'].to_i > 0 ? options['width_minimum'].to_i : WAVEFORM_WIDTH_MIN
+      width_per_second = options['width_per_second'].to_i
+      duration = AudioMonster.audio_file_duration(source_wav.path)
+      options['width'] = [(duration * width_per_second), min].max
+    end
+
+    wf = waveformjson(source_wav, options)
+
+    base_file_name = File.basename(source.path) + '.json'
+    temp_file = AudioMonster.create_temp_file(base_file_name, false)
+    temp_file.write wf.to_json
+    temp_file.fsync
+
+    self.destination = temp_file
+    self.destination_format = 'json'
+    info = info_for_waveform(wf)
+
+    completed info, 'waveformjson for audio complete'
+  end
+
+  def info_for_waveform(data)
+    { data_count: data.size }
+  end
+
+  def waveformjson(wav, opts={})
+    Waveformjson.generate(wav, symbolize_options(opts))
+  end
+
+  # task methods of "#{task_type}_#{job_type}" naming standard
+  def transcode_audio
+    format = options['format'].to_s
+    raise "Unsupported format: '#{format}'" unless SUPPORTED_FORMATS.include?(format)
+
+    self.destination = transcode_to(format, options)
+    self.destination_format = format
+    info = info_for(destination_format, destination)
+
+    completed info, 'transcode complete'
+  end
+
+  def copy_audio
+    self.destination = source
+    self.destination_format = source_format
+    info = info_for(destination_format, destination)
+
+    completed info, 'copy complete'
+  end
+
+  def analyze_audio
+    info = info_for(source_format, source)
+    completed info, 'analysis complete'
+  end
+
+  # this is a stub for the moment - not validating anything yet
+  def validate_audio
+    errors, analysis = validate(source_format, source, options)
+    info = { errors: errors, analysis: analysis }
+
+    completed info, 'validation complete'
+  end
+
+  def cut_audio
+    source_wav = get_wav_from_source
+    task_tmp = AudioMonster.create_temp_file(File.basename(source.path))
+    AudioMonster.cut_wav(source_wav.path, task_tmp.path, options['length'], options['fade'] || 5 )
+
+    self.destination_format = 'wav'
+    self.destination = task_tmp
+    info = info_for(destination_format, destination)
+
+    completed info, 'cut audio complete'
+  end
+
+  def slice_audio
+    source_wav = get_wav_from_source
+    task_tmp = AudioMonster.create_temp_file(File.basename(source.path))
+    finish = options['finish'].blank? ? '' : "=#{options['finish']}"
+    AudioMonster.slice_wav(source_wav.path, task_tmp.path, options['start'], (options['length'] || finish))
+
+    self.destination_format = 'wav'
+    self.destination = task_tmp
+    info = info_for(destination_format, destination)
+
+    completed info, 'slice audio complete'
+  end
+
+  def tone_detect_audio
+    # get a wave for it
+    source_wav = get_wav_from_source
+    tone = options['tone'] || 25
+    threshold = options['threshold'] || 0.05
+    min_time = options['min_time'] || 0.5
+    ranges = AudioMonster.tone_detect(source_wav.path, tone, threshold, min_time)
+    info = ranges.collect{|r| [r[:start], r[:finish]] }
+
+    completed info, 'tone detection complete'
+  end
+
+  def silence_detect_audio
+    # get a wave for it
+    source_wav = get_wav_from_source
+    threshold = options['threshold'] || 0.005
+    min_time = options['min_time'] || 1.0
+    # puts "t: #{threshold}, m: #{min_time}"
+    ranges = AudioMonster.silence_detect(source_wav.path, threshold, min_time)
+    info = ranges.collect{|r| [r[:start], r[:finish]] }
+
+    completed info, 'silence detection complete'
+  end
+
+  def wrap_audio
+    task_tmp = AudioMonster.create_temp_file(File.basename(source.path))
+    if source_format == 'wav'
+      AudioMonster.add_cart_chunk_to_wav(source.path, task_tmp.path, options)
+    elsif ['mp2', 'mp3'].include?(source_format)
+      AudioMonster.create_wav_wrapped_mp2(source.path, task_tmp.path, options)
+    end
+
+    self.destination_format = 'wav'
+    self.destination = task_tmp
+    info = info_for(destination_format, destination)
+
+    completed info, 'wav wrapped with cart chunk'
+  end
+
+  def extract_result_options(result_uri_string)
+    uri, options = super
+
+    if uri.scheme == 's3'
+      if @result_details[:info] && @result_details[:info][:content_type]
+        options[:content_type] = @result_details[:info][:content_type]
+      end
+    end
+
+    return [uri, options]
+  end
+
+  def symbolize_options(options)
+    Hash[options.map{|(k,v)| [k.to_sym,v]}]
+  end
+
+  def validate(format, file, opts)
+    if format == 'mp2'
+      AudioMonster.send("validate_#{format}".to_sym, file.path, opts)
+    else
+      raise "Format: #{format} not supported. Only validating mp2 is currently supported."
+    end
+  end
+
+  # helper methods
+  def info_for(format, file)
+    AudioMonster.send("info_for_#{format}".to_sym, file.path)
+  end
+
+  def transcode_to(format, opts)
+    # if the requested format is wav, well, we are done, short circuit here
+    if format == 'wav'
+      transcode_tmp = get_wav_from_source
+    else
+      transcode_tmp = AudioMonster.create_temp_file(File.basename(source.path + ".#{format}"))
+      AudioMonster.send("encode_#{format}_from_wav", get_wav_from_source.path, transcode_tmp.path, opts)
+    end
+    transcode_tmp
+  end
+
+  def get_wav_from_source
+    if !wav_pcm_tempfile && source_format == 'wav'
+      wav = NuWav::WaveFile.parse(source.path)
+      if wav.is_pcm?
+        self.wav_pcm_tempfile = source
+      else
+        logger.info "#{source.path} is not a pcm wav."
+      end
+    end
+
+    if !wav_pcm_tempfile
+      wav_tmp = AudioMonster.create_temp_file(File.basename(source.path + ".wav"))
+      AudioMonster.send("encode_wav_pcm_from_#{source_format}".to_sym, source.path, wav_tmp.path)
+      self.wav_pcm_tempfile = wav_tmp
+    end
+
+    wav_pcm_tempfile
+  end
+
+  def prepare_task
+    release_tempfile(wav_pcm_tempfile)
+    self.wav_pcm_tempfile = nil
+  end
+
+  def complete_task
+    release_tempfile(wav_pcm_tempfile)
+    self.wav_pcm_tempfile = nil
+
+    release_tempfile(source)
+    self.source = nil
+
+    release_tempfile(original)
+    self.original = nil
+  end
+
+  def release_tempfile(tmp)
+    if(tmp && tmp.is_a?(Tempfile))
+      logger.debug "release_wav_tmp: start #{tmp.path}"
+      tmp.close
+      tmp.unlink
+      logger.debug "release_wav_tmp: end #{tmp.path}"
+    end
+  rescue Exception => err
+    logger.error "release_wav_tmp: err #{err.inspect}"
+  end
+end
---
+++ test/processors/audio_processor_test.rb
@@ -0,0 +1,64 @@
+require 'test_helper'
+
+class AudioProcessorTest < ActiveSupport::TestCase
+
+  # let(:processor) { AudioProcessor.new(logger: Logger.new(STDOUT)) }
+  let(:processor) { AudioProcessor.new(logger: Logger.new('/dev/null')) }
+
+  it 'defines supported tasks' do
+    AudioProcessor.supported_tasks.first.must_equal 'transcode'
+  end
+
+  it 'can symbolize hash' do
+    processor.symbolize_options({'foo' => 'bar'})[:foo].must_equal 'bar'
+  end
+
+  describe 'slice_audio' do
+
+    let(:msg) {
+      {
+        task: {
+          id: 'guid1',
+          task_type: 'slice',
+          label: 'cuisanart',
+          job: { id: 'guid2', job_type: 'audio', status: 'created', original: "file://#{in_file('test_long.wav')}" },
+          options: { start: '10', length: '5' }
+        }
+      }.with_indifferent_access
+    }
+
+    it 'should return sliced duration' do
+      processor.on_message(msg)
+      processor.result_details[:info][:length].to_i.must_equal 5
+    end
+  end
+
+  describe 'tone_detect_audio' do
+
+    let(:msg) {
+      {
+        task: {
+          id: 'guid3',
+          task_type: 'tone_detect',
+          label: 'tone_detect',
+          job: { id: 1, job_type: 'audio', status: 'created', original: "file://#{in_file('test.flac')}" },
+          options: { frequency: '25' }
+        }
+      }.with_indifferent_access
+    }
+
+    it 'should return one tone at 5-6 seconds' do
+      processor.on_message(msg)
+
+      # puts 'tones:'
+      # processor.result_details[:info].each{|t| puts "#{t[:start].to_i.to_time_string_summary} - #{t[:finish].to_i.to_time_string_summary}" }
+
+      # puts processor.result_details.inspect
+      processor.result_details[:info].size.must_equal 1
+      processor.result_details[:info].first[0].to_i.must_equal 5
+      processor.result_details[:info].first[1].to_i.must_equal 6
+    end
+
+  end
+
+end
