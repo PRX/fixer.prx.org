927e65d47d9ee78543985cd315fc49a068f9527e
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-19 12:47:38 -0500

    get update task log worker going

--- app/workers/task_update_worker.rb
+++ app/workers/task_update_worker.rb
@@ -3,6 +3,12 @@
 require 'base_worker'
 
 class TaskUpdateWorker < BaseWorker
-  def process(log)
+  def perform(log)
+    task_log = log['task_log']
+    task = Task.find_by_id(task_log['task_id'])
+    return unless task
+
+    logged_at = task_log['logged_at'].blank? ? Time.now : Time.parse(task_log['logged_at'])
+    task.log(task_log['status'], task_log['message'], task_log['info'], logged_at)
   end
 end
---
+++ test/workers/task_update_worker_test.rb
@@ -0,0 +1,33 @@
+require 'test_helper'
+
+class TaskUpdateWorkerTest < ActiveSupport::TestCase
+
+  before {
+    ENV['FIXER_WORKER_LIB'] = 'local'
+  }
+
+  let(:job) { Job.create!(job_type: 'audio', priority: 1, application_id: 1) }
+  let(:task) { Task.create!(job: job) }
+
+  let(:msg) do
+    l = {
+      task_log: {
+        logged_at: Time.now,
+        task_id: task.id,
+        status: 'processing',
+        message: 'Processing started!',
+        info: {
+          started_at: Time.now
+        },
+      }
+    }
+    JSON.parse(l.to_json)
+  end
+
+  it 'calls worker perform' do
+    log = TaskUpdateWorker.publish('test-queue', msg)
+    log.must_be_instance_of TaskLog
+    log.task_id.must_equal task.id
+    log.status.must_equal 'processing'
+  end
+end
