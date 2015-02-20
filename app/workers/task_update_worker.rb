9957f4b8ab734897042bbf5c82cf2abaee95eed8
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-20 17:36:42 -0500

    fix bug in setting logged_at, switch to symbol keys

--- app/workers/task_update_worker.rb
+++ app/workers/task_update_worker.rb
@@ -3,13 +3,23 @@
 require 'base_worker'
 
 class TaskUpdateWorker < BaseWorker
+
   def perform(log)
     log = log.with_indifferent_access
-    task_log = log['task_log']
-    task = Task.find_by_id(task_log['task_id'])
+    task_log = log[:task_log].with_indifferent_access
+    task = Task.find_by_id(task_log[:task_id])
     return unless task
 
-    logged_at = task_log['logged_at'].blank? ? Time.now : Time.parse(task_log['logged_at'])
-    task.log(task_log['status'], task_log['message'], task_log['info'], logged_at)
+    task.log(task_log[:status], task_log[:message], task_log[:info], get_logged_at(task_log))
+  end
+
+  def get_logged_at(task_log)
+    if task_log[:logged_at].blank?
+      Time.now
+    elsif task_log[:logged_at].instance_of?(String)
+      Time.parse(task_log[:logged_at])
+    else
+      task_log[:logged_at]
+    end
   end
 end
