31622d68defda2751a78ed70a588a032a3ab410d
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-24 16:43:58 -0500

    use a conn pool for db access

--- app/workers/web_hook_update_worker.rb
+++ app/workers/web_hook_update_worker.rb
@@ -4,12 +4,14 @@ require 'base_worker'
 
 class WebHookUpdateWorker < BaseWorker
 
-  def perform(web_hook)
-    log = log.with_indifferent_access
-    web_hook_log = log[:web_hook]
-    web_hook = WebHook.find_by_id(web_hook_log[:id])
-    return unless web_hook
+  def perform(log)
+    ActiveRecord::Base.connection_pool.with_connection do
+      log = log.with_indifferent_access
+      web_hook_log = log[:web_hook].with_indifferent_access
+      web_hook = WebHook.find_by_id(web_hook_log[:id])
+      return unless web_hook
 
-    web_hook.update_completed(web_hook_log[:complete])
+      web_hook.update_completed(web_hook_log[:complete])
+    end
   end
 end
