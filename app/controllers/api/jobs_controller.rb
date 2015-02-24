a40a38e539f65841e78fef1a900260108a6c7757
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-24 16:26:33 -0500

    handle strong params on jobs

--- app/controllers/api/jobs_controller.rb
+++ app/controllers/api/jobs_controller.rb
@@ -13,13 +13,14 @@ module Api
     end
 
     def update
-      tasks = params[:job].delete('tasks') || []
-      @job.update_attributes!(params[:job])
+      job_attributes = job_params
+      tasks = job_attributes.delete(:tasks) || []
+      @job.update_attributes!(job_attributes)
       respond_with @job
     end
 
     def create
-      @job = Job.create_from_message(params, current_application)
+      @job = Job.create_from_message(job_params, current_application)
       respond_with @job
     end
 
@@ -33,6 +34,14 @@ module Api
     def set_job
       @job = Job.find(params[:id])
       raise 'Not your job!' unless @job.application == current_application
+    end
+
+    def job_params
+      option_keys = params[:job][:tasks].map{ |t| (t[:options] || {}).keys }.flatten
+      logger.error "option_keys: #{option_keys.inspect}"
+      task_attributes = [:task_type, :result, :label, :call_back, options: option_keys]
+      job_attrs = [:job_type, :original, :status, :call_back, :priority, :retry_max, :retry_delay, tasks: task_attributes]
+      params.require(:job).permit(job_attrs)
     end
   end
 end
