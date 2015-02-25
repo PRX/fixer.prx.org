45186a23e1656fcd5f469e350e1be745987accb9
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-25 18:42:29 -0500

    use env for sidekiq db conn pool, add default worker options

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,8 +1,16 @@
-# config/initializers/sidekiq.rb
+if ENV['WORKER_LIB'] == 'sidekiq'
+
+require 'sidekiq'
+
 Sidekiq.configure_server do |config|
   database_url = ENV['DATABASE_URL']
   if database_url
-    ENV['DATABASE_URL'] = "#{database_url}?pool=25"
+    ENV['DATABASE_URL'] = "#{database_url}?pool=#{ENV['SIDEKIQ_DATABASE_POOL_SIZE']}"
     ActiveRecord::Base.establish_connection
   end
+end
+
+
+Sidekiq.default_worker_options = { 'backtrace' => 10 }
+
 end
