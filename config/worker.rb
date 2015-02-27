90f19717884ec3ac80f32f66db6a74363f132110
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-02-27 09:18:57 -0500

    first cut at loading worker process without rails

---
+++ config/worker.rb
@@ -0,0 +1,20 @@
+require 'dotenv'
+Dotenv.load
+
+app_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
+ENV['APP_ROOT'] ||= app_root
+$:.unshift(app_root)
+
+# set temp file locations
+ENV['NU_WAV_TMP_DIR'] ||= File.join(app_root, 'tmp', 'nu_wav')
+ENV['GOOGLE_SPEECH_TMP_DIR'] ||= File.join(app_root, 'tmp', 'google_speech')
+
+# load worker initializer
+require File.join(app_root, "config/initializers/#{ENV['WORKER_LIB']}.rb")
+
+# load the code
+['lib', 'app/processors', 'app/workers/concerns', 'app/workers'].each do |path|
+  ruby_path = File.join(app_root, path)
+  $:.unshift(ruby_path)
+  Dir["#{ruby_path}/**/*.rb"].each {|file| require file }
+end
