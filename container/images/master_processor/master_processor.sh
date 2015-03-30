08e578e5a37f04cc4dab2fb04cbfabf573f0c90a
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-03-30 18:25:37 -0400

    using wrong config file

--- container/images/master_processor/master_processor.sh
+++ container/images/master_processor/master_processor.sh
@@ -2,4 +2,5 @@
 set -e
 
 cd $APP_HOME
-exec /sbin/setuser app bundle exec $WORKER_LIB -R -C $APP_HOME/config/$WORKER_LIB/worker.yml 2>&1
+exec /sbin/setuser app bundle exec $WORKER_LIB -R -C $APP_HOME/config/$WORKER_LIB/master.yml 2>&1
+
