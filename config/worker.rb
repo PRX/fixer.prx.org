4f1553bb849ea9277db60b0d9eded4edb5856e07
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-03-30 11:12:56 -0400

    update worker to load dotenv only in dev

--- config/worker.rb
+++ config/worker.rb
@@ -1,5 +1,7 @@
-require 'dotenv'
-Dotenv.load
+if ENV['RAILS_ENV'] == 'development'
+  require 'dotenv'
+  Dotenv.load
+end
 
 app_root = File.expand_path(File.join(File.dirname(__FILE__), '..'))
 ENV['APP_ROOT'] ||= app_root
