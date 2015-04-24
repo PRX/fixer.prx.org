011dcd139f2a8f3856a13cf08fbf04548c7d0449
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-04-24 12:29:52 -0400

    simplify test based on removed code

--- test/workers/base_worker_test.rb
+++ test/workers/base_worker_test.rb
@@ -3,25 +3,19 @@ require 'test_helper'
 class BaseWorkerTest < ActiveSupport::TestCase
 
   class EchoWorker < BaseWorker
+    queue_as :test_queue
+
     def perform(*args)
-      args.first[:echo]
+      e = JSON.parse(args.first)
+      e['echo']
     end
   end
 
-  before {
-    ENV['WORKER_LIB'] = 'local'
-  }
-
   it 'knows the worker lib' do
-    BaseWorker.worker_lib.must_equal 'local'
-  end
-
-  it 'can configure options' do
-    BaseWorker.worker_options({foo: 'bar'})
-    BaseWorker.get_local_options[:foo].must_equal 'bar'
+    BaseWorker.worker_lib.must_equal 'inline'
   end
 
   it 'calls worker perform' do
-    EchoWorker.publish('test-queue', {echo: 'bar'}, {}).must_equal 'bar'
+    EchoWorker.new.perform({echo: 'bar'}.to_json).must_equal 'bar'
   end
 end
