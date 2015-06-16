92accb97f7c9c36f70bf5d1749af3baddb52cbbf
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-06-16 13:40:45 -0400

    change fk to be a string to handle uuids

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150611174830) do
+ActiveRecord::Schema.define(version: 20150615205022) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -100,7 +100,7 @@ ActiveRecord::Schema.define(version: 20150611174830) do
     t.string   "job_method"
     t.text     "data"
     t.string   "scheduled_type"
-    t.integer  "scheduled_id"
+    t.string   "scheduled_id"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
   end
