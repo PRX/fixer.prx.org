17615cfa4b7344391e6b6fd71223761767ef8c5b
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-06-22 13:57:16 -0400

    use default service options for sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,9 +1,18 @@
 if ENV['WORKER_LIB'] == 'sidekiq'
 
 require 'sidekiq'
+require 'service_options'
 
-if defined?(ActiveRecord)
-  Sidekiq.configure_server do |config|
+redis_options = ServiceOptions.service_options(:redis).symbolize_keys || {}
+
+Sidekiq.configure_client do |config|
+  config.redis = redis_options
+end
+
+Sidekiq.configure_server do |config|
+  config.redis = redis_options
+
+  if defined?(ActiveRecord)
     database_url = ENV['DATABASE_URL']
     if database_url
       ENV['DATABASE_URL'] = "#{database_url}?pool=#{ENV['SIDEKIQ_DATABASE_POOL_SIZE']}"
