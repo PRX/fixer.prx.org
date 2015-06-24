d3b6ba8b04f2a7a1d773981d9bb434d6f8dc72b6
Andrew Kuklewicz <andrew@beginsinwonder.com>
2015-06-24 14:39:13 -0400

    allow worker to be configured, structure message correctly

--- app/workers/callbacks/sns_callback.rb
+++ app/workers/callbacks/sns_callback.rb
@@ -1,23 +1,40 @@
 # encoding: utf-8
 
-require 'fog'
+require 'rack'
+require 'aws-sdk-core'
 require 'service_options'
 
 module SnsCallback
   def sns_callback(web_hook)
     uri = URI.parse(web_hook[:url])
     options = sns_options(uri)
+    params = Rack::Utils.parse_nested_query(uri.query)
 
     sns = sns(options)
     destination = uri.path[1..-1]
     arn = find_or_create_topic(sns, destination)
 
-    sns.publish(topic_arn: arn, message: web_hook[:message])
+    sns.publish(message(arn, web_hook, params['worker']))
   end
 
   def find_or_create_topic(sns, name)
     resp = sns.create_topic(name: name)
     resp.topic_arn
+  end
+
+  def message(arn, web_hook, worker)
+    msg = {
+      topic_arn: arn,
+      message: web_hook[:message],
+      message_attributes: {}
+    }
+    if worker
+      msg[:message_attributes]['shoryuken_class'] = {
+        string_value: worker,
+        data_type: 'String'
+      }
+    end
+    msg
   end
 
   def sns(options = {})
